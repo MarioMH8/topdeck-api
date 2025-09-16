e851311f359b429b6891103ea425c0f41ad4c8b1
MarioMH8 <mhidalgomario@gmail.com>
2025-09-16 15:59:45 +0200

    feat: fix deckList

--- src/schema/deck-list/deck-list.schema.ts
+++ src/schema/deck-list/deck-list.schema.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
+const URL_REGEX = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&/=]*/g;
+
 const DeckListSchema = z.coerce.string().transform(value => {
 	if (!value) {
 		return;
 	}
 
-	const parsed = z.url().safeParse(value).data;
+	const extracted = URL_REGEX.exec(value);
+
+	const url = extracted ? extracted[0] : value;
+
+	const parsed = z.url().safeParse(url).data;
 	if (parsed === 'https://www.moxfield.com/decks/undefined') {
 		return;
 	}
